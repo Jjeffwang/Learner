27b77088e0392128dd38b907b4da197a11ef4e43
Jjeffwang <2417233312@qq.com>
2017-11-21 22:47:11 +0800

    test

---
+++ src/main/java/multi/thread/lock/Miantet.java
@@ -0,0 +1,12 @@
+package multi.thread.lock;
+
+import com.sun.org.apache.xerces.internal.dom.PSVIAttrNSImpl;
+
+/**
+ * Created by jeff on 2017/11/21.
+ */
+public class Miantet {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
