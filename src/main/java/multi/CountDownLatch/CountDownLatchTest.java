b5b135291af2a6bf0bfdfb7d58541c281834ef0d
Jjeffwang <2417233312@qq.com>
2017-12-04 18:00:03 +0800

    并发工具类

---
+++ src/main/java/multi/CountDownLatch/CountDownLatchTest.java
@@ -0,0 +1,25 @@
+package multi.CountDownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by ${WangChengYong} on 2017/12/4.
+ */
+public class CountDownLatchTest {
+
+    static CountDownLatch countDownLatch=new CountDownLatch(2);
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("1");
+                countDownLatch.countDown();
+                System.out.println("2");
+                countDownLatch.countDown();
+            }
+        }).start();
+        //await()会阻塞当前线程直到CountDownLatch中计数为0；
+        countDownLatch.await();
+        System.out.println("3");
+    }
+}
---
+++ src/main/java/multi/CyclicBarrier/BankWaterService.java
@@ -0,0 +1,58 @@
+package multi.CyclicBarrier;
+
+import java.util.Map;
+import java.util.concurrent.*;
+
+/**
+ * 银行流水处理服务类
+ * Created by ${WangChengYong} on 2017/12/4.
+ */
+public class BankWaterService implements Runnable {
+
+    /**
+     * 创建4个屏障，处理完之后执行当前类的run方法
+     */
+    private CyclicBarrier cyclicBarrier = new CyclicBarrier(4, this);
+    /**
+     * 启动4个线程
+     */
+    private Executor executor = Executors.newFixedThreadPool(4);
+
+    private ConcurrentHashMap<String, Integer> countMap = new ConcurrentHashMap<String, Integer>();
+
+    private void count() {
+        for (int i = 0; i < 4; i++) {
+            executor.execute(new Runnable() {
+                @Override
+                public void run() {
+                    countMap.put(Thread.currentThread().getName(),1);
+                    try {
+                        //4个线程计算4次，没计算完成一次插入一个屏障
+                        cyclicBarrier.await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        }
+    }
+
+    @Override
+    public void run() {
+        int result =0;
+        for(Map.Entry<String,Integer> sheet:countMap.entrySet()){
+            result+=sheet.getValue();
+        }
+        countMap.put("result",result);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        BankWaterService bankWaterService=new BankWaterService();
+        bankWaterService.count();
+    }
+
+}
